4fec14ab7294128717e633a0fd5b2dbe69ca90b8
James Anh <james.anh184@gmail.com>
2022-07-07 23:18:48 -0700

    modified js files

---
+++ __test__/Employee.test.js
@@ -0,0 +1,24 @@
+// require Employee folder
+const Employee = require("../lib/Employee")
+// describe(name, fn) creates a block that groups together several related tests. For example, if you have a myBeverage object that is supposed to be delicious but not sour, you could test it with:
+// .describe examples 
+
+// const myBeverage = {
+//   delicious: true,
+//   sour: false,
+// };
+
+// describe('my beverage', () => {
+//   test('is delicious', () => {
+//     expect(myBeverage.delicious).toBeTruthy();
+//   });
+
+//   test('is not sour', () => {
+//     expect(myBeverage.sour).toBeFalsy();
+//   });
+// });
+
+// test for Employee
+describe('Employee', () => {
+    // 
+})
---
+++ index.js
@@ -0,0 +1,11 @@
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs")
+const inquirer = require("inquirer");
+const generateHTML = require("./dist/GenerateHTML");
+
+
+
+
